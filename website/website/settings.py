c90a0a92a622a4a4bdf94a1774ca3969aa3fbf1e
krsm <krsm184@gmail.com>
2017-01-20 19:50:02 -0500

    added database music models

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # custom apps
+    'music.apps.MusicConfig',
+    # ------
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
