4afe27ff2c0c81e1c282b23ba7680ae9b7db9df8
krsm <krsm184@gmail.com>
2017-02-05 18:31:29 -0500

    adding templates

--- website/music/views.py
+++ website/music/views.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse
+# to work with templates
+from django.templates import loader
+
 from .models import Album
+
 
 #homepage
 def index(request):
     # connect to datbase and parse data
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/'+ str(album.id) +'/'
-        html += '<a href="' + url +'">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+            'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 # details view
 def detail(request, album_id):
